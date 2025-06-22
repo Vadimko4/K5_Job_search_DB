a5860821d5cf83abaf0d8c616886ed9d2e5aa0e2
vadimko4 <vad.informat@gmail.com>
2025-06-22 15:52:51 +0400

    add HhHandler with get_employers_info

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from configparser import ConfigParser
 
 employers = [9694561, 5331842, 816144,
              87021, 1702765, 2180,
-             15478, 3529, 864643, 1740]
+             15478, 3529, 4905190, 1740]
 
 
 def config(filename='database.ini', section='postgresql'):
--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print(111)
+from src.hh_handler import HhHandler
+
+if __name__ == '__main__':
+    my_hh = HhHandler()
+    my_hh.get_employers_info()
+    for emp in my_hh.employers:
+        print(emp)
+        input()
+
