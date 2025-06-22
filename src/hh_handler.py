b4bf38d08e78c94b437cde53d63973415b1aa0bb
vadimko4 <vad.informat@gmail.com>
2025-06-22 18:36:16 +0400

    add get_vacancies_info

---
+++ src/hh_handler.py
@@ -0,0 +1,114 @@
+"""Модуль с классом для работы с API HH"""
+from http.client import responses
+
+import requests
+from src.config import employers
+
+
+class ApiError(Exception):
+    pass
+
+
+class NotFoundError(ApiError):
+    pass
+
+
+class ServerError(ApiError):
+    pass
+
+
+class HhHandler:
+    """
+    Класс для работы с API HeadHunter
+    """
+
+    def __init__(self):
+        """Конструктор класса"""
+        self.url_emp = 'https://api.hh.ru/employers'
+        self.url_vac = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'page': 0, 'per_page': 10} # получаем до 10 х 20 = 200 вакансий от каждой компании работодателя
+        self.employers = []
+        self.vacancies = []
+
+    def get_employers_info(self):
+        """
+        Получение информации о работодателях и количестве их вакансий
+        с hh.ru - в формате списка словарей
+        """
+        for employer_id in employers:
+            response = requests.get(f'{self.url_emp}/{employer_id}')
+            self.employers.append(
+                {
+                    'emp_hh_id': employer_id,
+                    'emp_name': response.json()['name'],
+                    'emp_url': response.json()['alternate_url'],
+                    'vac_count': response.json()['open_vacancies']
+            })
+
+    def get_vacancies_info(self):
+        """
+        Получение информации о всех вакансиях отслеживаемых 10-ти работодателей
+        с hh.ru в формате JSON - в формате списка словарей
+        """
+        for employer_id in employers:
+            self.params['employer_id'] = employer_id
+
+            while self.params.get('page') != 20:
+                response = requests.get(self.url_vac, headers=self.headers, params=self.params)
+                status_code = response.status_code
+
+                if 300 > status_code >= 200:
+                    vacancies = response.json()['items']
+
+                    # вытаскиваем из списка словарей с вакансиями только нужную нам информацию
+                    new_vacancies = []
+                    for vacancy_dict in vacancies:
+                        name = vacancy_dict['name']
+                        link = vacancy_dict['alternate_url']
+                        salary_info = vacancy_dict['salary']
+                        # по поводу зарплаты рассматриваем разные случаи
+                        if not salary_info:
+                            salary_from = 0
+                            salary_to = 0
+                        else:
+                            if vacancy_dict['salary']['from'] is None:
+                                salary_from = 0
+                            else:
+                                salary_from = vacancy_dict['salary']['from']
+                            if vacancy_dict['salary']['to'] is None:
+                                salary_to = 0
+                            else:
+                                salary_to = vacancy_dict['salary']['to']
+                        description = vacancy_dict['snippet']['requirement']
+
+                        # формируем нужный нам словарь о вакансии
+                        new_vacancy_dict = dict()
+                        new_vacancy_dict['employer_id'] = employer_id
+                        new_vacancy_dict['vacancy_name'] = name
+                        new_vacancy_dict['link'] = link
+                        new_vacancy_dict['salary_from'] = salary_from
+                        new_vacancy_dict['salary_to'] = salary_to
+                        new_vacancy_dict['description'] = description
+
+                        new_vacancies.append(new_vacancy_dict)
+
+                    self.vacancies.extend(new_vacancies)
+                    self.params['page'] += 1
+
+                elif 500 > status_code >= 400:
+                    print(status_code)
+                    print(response.json())
+                    raise NotFoundError("Запрос содержит ошибку или неверные данные")
+
+                elif 600 > status_code >= 500:
+                    raise ServerError("На стороне сервера произошла ошибка при обработке запроса")
+
+            self.params['page'] = 0
+            print(len(self.vacancies))
+            input()
+
+    def erase_old_vacancies(self):
+        """Стирает ранее найденные вакансии для нового поиска"""
+        self.vacancies = []
+        self.params['page'] = 0
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.hh_handler import HhHandler
 if __name__ == '__main__':
     my_hh = HhHandler()
     my_hh.get_employers_info()
-    for emp in my_hh.employers:
-        print(emp)
-        input()
-
+    # for emp in my_hh.employers:
+    #     print(emp)
+    my_hh.get_vacancies_info()
+    print(len(my_hh.vacancies))
